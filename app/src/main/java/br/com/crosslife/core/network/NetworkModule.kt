685edc99cb74c704861ea9236566d03a64a5a3b3
devLucasYuji <lucasyujideveloper@gmail.com>
2021-07-25 17:10:40 -0300

    Create service to Weekly add interceptor to add authorization header

--- app/src/main/java/br/com/crosslife/core/network/NetworkModule.kt
+++ app/src/main/java/br/com/crosslife/core/network/NetworkModule.kt
@@ -2,11 +2,14 @@ package br.com.crosslife.core.network
 
 import br.com.crosslife.BuildConfig
 import br.com.crosslife.core.network.services.UserService
-import com.squareup.moshi.Moshi
+import br.com.crosslife.core.network.services.WeeklyTrainService
+import br.com.crosslife.domain.preferences.UserStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -16,7 +19,18 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private val okHttpClient get() = OkHttpClient.Builder().build()
+    fun okHttpClient(userStore: UserStore): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor {
+                runBlocking {
+                    val token = userStore.getToken().first()
+                    val request = it.request().newBuilder()
+                        .addHeader("Authorization", token)
+                        .build()
+                    it.proceed(request)
+                }
+            }.build()
+    }
 
     private inline fun <reified T> newInstance(retrofit: Retrofit): T {
         return retrofit.create(T::class.java)
@@ -24,13 +38,17 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(dataStore: UserStore): Retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
-        .client(okHttpClient)
+        .client(okHttpClient(dataStore))
         .build()
 
     @Singleton
     @Provides
     fun provideUserService(retrofit: Retrofit): UserService = newInstance(retrofit)
+
+    @Singleton
+    @Provides
+    fun provideWeeklyTrainService(retrofit: Retrofit): WeeklyTrainService = newInstance(retrofit)
 }
