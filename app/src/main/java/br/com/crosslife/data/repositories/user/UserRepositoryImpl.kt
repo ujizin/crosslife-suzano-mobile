78837ec8b52ac3e17d4c106f89c3503d5e475c27
devLucasYuji <lucasyujideveloper@gmail.com>
2021-07-26 00:24:32 -0300

    check if username or password is empty on repository

--- app/src/main/java/br/com/crosslife/data/repositories/user/UserRepositoryImpl.kt
+++ app/src/main/java/br/com/crosslife/data/repositories/user/UserRepositoryImpl.kt
@@ -2,6 +2,7 @@ package br.com.crosslife.data.repositories.user
 
 import br.com.crosslife.core.network.payload.UserPayload
 import br.com.crosslife.core.network.services.UserService
+import br.com.crosslife.data.errors.EmptyError
 import br.com.crosslife.domain.models.User
 import br.com.crosslife.domain.preferences.UserStore
 import br.com.crosslife.domain.repositories.UserRepository
@@ -14,6 +15,7 @@ class UserRepositoryImpl(
 ) : UserRepository {
 
     override fun fetchLogin(username: String, password: String): Flow<User> = flow {
+        check(username.isNotEmpty() && password.isNotEmpty()) { throw EmptyError() }
         val userPayload = UserPayload(username, password)
         val userDto = userService.fetchLogin(userPayload)
 
