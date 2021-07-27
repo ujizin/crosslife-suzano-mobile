70bee88f8e467c02933db1d65959beeb090b4cd2
devLucasYuji <lucasyujideveloper@gmail.com>
2021-07-27 00:18:16 -0300

    refactor enum Screen to sealed classes

--- app/src/main/java/br/com/crosslife/Screen.kt
+++ app/src/main/java/br/com/crosslife/Screen.kt
@@ -3,25 +3,29 @@ package br.com.crosslife
 import br.com.crosslife.ui.components.bottomnavigation.Tab
 
 
-enum class Screen(val route: String, val tabItem: Tab = Tab.Home) {
-    Splash(Screen.SPLASH_PATH, Tab.None),
-    Login(Screen.LOGIN_PATH, Tab.None),
-    ForgotPassword(Screen.FORGOT_PASSWORD_PATH, Tab.None),
+sealed class Screen(val route: String, val tabItem: Tab = Tab.Home) {
+    object Splash : Screen(SPLASH_PATH, Tab.None)
+    object Login : Screen(LOGIN_PATH, Tab.None)
+    object ForgotPassword : Screen(FORGOT_PASSWORD_PATH, Tab.None)
 
-    Home(Screen.HOME_PATH, Tab.Home),
-    Search(Screen.SEARCH_PATH, Tab.Search),
-    Chat(Screen.CHAT_PATH, Tab.Chat),
-    Profile(Screen.PROFILE_PATH, Tab.Profile),
-    DetailProfile(Screen.DETAIL_PROFILE_PATH, Tab.None),
-    ChangePassword(Screen.CHANGE_PASSWORD_PATH, Tab.None),
+    object Home : Screen(HOME_PATH, Tab.Home)
+    object Search : Screen(SEARCH_PATH, Tab.Search)
+    object Chat : Screen(CHAT_PATH, Tab.Chat)
+    object Profile : Screen(PROFILE_PATH, Tab.Profile)
+    object DetailProfile : Screen(DETAIL_PROFILE_PATH, Tab.None)
+    object ChangePassword : Screen(CHANGE_PASSWORD_PATH, Tab.None)
 
-    Root(Screen.ROOT_PATH),
-    HomeRoot(Screen.HOME_ROOT_PATH),
-    SearchRoot(Screen.SEARCH_ROOT_PATH),
-    ProfileRoot(Screen.PROFILE_ROOT_PATH),
-    ChatRoot(Screen.CHAT_ROOT_PATH),
+    object Root : Screen(ROOT_PATH)
+    object HomeRoot : Screen(HOME_ROOT_PATH)
+    object SearchRoot : Screen(SEARCH_ROOT_PATH)
+    object ProfileRoot : Screen(PROFILE_ROOT_PATH)
+    object ChatRoot : Screen(CHAT_ROOT_PATH)
 
-    Logout(Screen.LOGOUT_PATH, Tab.Profile);
+    object WeeklyTrain : Screen("$WEEKLY_TRAIN_PATH/{id}") {
+        fun createRoute(id: Int) = "$WEEKLY_TRAIN_PATH/$id"
+    }
+
+    object Logout : Screen(LOGOUT_PATH, Tab.Profile)
 
     companion object {
         private const val SPLASH_PATH = "splash"
@@ -37,6 +41,8 @@ enum class Screen(val route: String, val tabItem: Tab = Tab.Home) {
         private const val PROFILE_ROOT_PATH = "profile_root"
         // End Tabs
 
+        private const val WEEKLY_TRAIN_PATH = "weekly_train"
+
         private const val HOME_PATH = "home"
         private const val SEARCH_PATH = "search"
         private const val CHAT_PATH = "chat"
@@ -47,8 +53,9 @@ enum class Screen(val route: String, val tabItem: Tab = Tab.Home) {
         private const val CHANGE_PASSWORD_PATH = "$PROFILE_PATH/change-password"
         // End Profile
 
-        fun findScreenByRoute(route: String?) = values().firstOrNull {
-            it.route == route
-        }
+        fun findScreenByRoute(route: String?) =
+            Screen::class.sealedSubclasses.firstOrNull {
+                it.objectInstance?.route == route
+            }?.objectInstance
     }
 }
