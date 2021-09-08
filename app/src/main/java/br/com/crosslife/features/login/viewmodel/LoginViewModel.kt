9e8740e77c0e12d72d244fc235214ffa49b5139a
devLucasYuji <lucasyujideveloper@gmail.com>
2021-09-08 00:34:12 -0300

    add logout flow on ui

--- app/src/main/java/br/com/crosslife/features/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/crosslife/features/login/viewmodel/LoginViewModel.kt
@@ -6,21 +6,28 @@ import br.com.crosslife.data.Result
 import br.com.crosslife.domain.repositories.UserRepository
 import br.com.crosslife.extensions.notify
 import br.com.crosslife.domain.models.User
+import br.com.crosslife.extensions.viewmodel.ViewModelExtensions
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val userRepository: UserRepository
-) : ViewModel() {
+    private val userRepository: UserRepository,
+) : ViewModel(), ViewModelExtensions {
 
-    private val _login = MutableStateFlow<Result<User>>(Result.Initial)
-    val login: StateFlow<Result<User>> get() = _login
+    val login: StateFlow<Result<User>> = MutableStateFlow(Result.Initial)
+    val logout: StateFlow<Result<Unit>> = MutableStateFlow(Result.Initial)
 
     fun fetchLogin(username: String, password: String) {
         userRepository.fetchLogin(username, password)
-            .notify(viewModelScope, _login)
+            .notify(viewModelScope, login())
+    }
+
+    fun fetchLogout() {
+        userRepository.fetchLogout()
+            .notify(viewModelScope, logout())
     }
 }
--- app/src/main/java/br/com/crosslife/features/logout/LogoutDialog.kt
+++ app/src/main/java/br/com/crosslife/features/logout/LogoutDialog.kt
@@ -4,18 +4,32 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import br.com.crosslife.R
 import br.com.crosslife.Screen
 import br.com.crosslife.components.Button
+import br.com.crosslife.data.Result
 import br.com.crosslife.extensions.capitalize
+import br.com.crosslife.features.login.viewmodel.LoginViewModel
 import br.com.crosslife.navigateAndPop
 import br.com.crosslife.ui.theme.Space
 
 @Composable
-fun NavController.LogoutDialog() {
+fun NavController.LogoutDialog(userViewModel: LoginViewModel = hiltViewModel()) {
+    val state by userViewModel.logout.collectAsState()
+    when (state) {
+        Result.Initial, Result.Loading -> LogoutScreen(userViewModel)
+        else -> navigateAndPop(Screen.Login, Screen.Home, true)
+    }
+}
+
+@Composable
+fun LogoutScreen(userViewModel: LoginViewModel) {
     Column(
         Modifier
             .padding(Space.XXS)
@@ -37,7 +51,7 @@ fun NavController.LogoutDialog() {
             horizontalArrangement = Arrangement.End,
         ) {
             Button(textButton = stringResource(id = R.string.logout).capitalize(), onClick = {
-                navigateAndPop(Screen.Login, Screen.Home, true)
+                userViewModel.fetchLogout()
             })
         }
     }
