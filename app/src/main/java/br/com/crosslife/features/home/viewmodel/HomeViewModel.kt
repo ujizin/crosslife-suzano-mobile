6026ba4a6c3063a5c5c63c7af0c5207e24096b33
devLucasYuji <lucasyujideveloper@gmail.com>
2021-09-19 20:02:33 -0300

    add refresh to home viewmodel

--- app/src/main/java/br/com/crosslife/features/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/br/com/crosslife/features/home/viewmodel/HomeViewModel.kt
@@ -10,6 +10,9 @@ import br.com.crosslife.extensions.viewmodel.ViewModelExtensions
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,17 +20,31 @@ class HomeViewModel @Inject constructor(
     private val weeklyTrainRepository: WeeklyTrainRepository,
 ) : ViewModel(), ViewModelExtensions {
 
+    val isRefresh: StateFlow<Boolean> = MutableStateFlow(false)
+
     val weeklyTrains: StateFlow<Result<List<WeeklyTrain>>> = MutableStateFlow(Result.Initial)
 
     // TODO add notices endpoint
     val notices: StateFlow<Result<WeeklyTrain>> = MutableStateFlow(Result.Initial)
 
     init {
+        viewModelScope.launch {
+            isRefresh.collect { isRefreshing ->
+                if (isRefreshing) {
+                    getWeeklyTrains()
+                        .onCompletion { isRefresh().value = false }
+                        .collect()
+                }
+            }
+        }
+
         getWeeklyTrains()
     }
 
-    private fun getWeeklyTrains() {
-        weeklyTrainRepository.fetchWeeklyTrains()
-            .notify(viewModelScope, weeklyTrains())
+    private fun getWeeklyTrains() = weeklyTrainRepository.fetchWeeklyTrains()
+        .notify(viewModelScope, weeklyTrains())
+
+    fun refresh() {
+        isRefresh().value = true
     }
 }
