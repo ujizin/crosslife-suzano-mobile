f01de75c9a7e884d3071cb46e3aff39c7d2da558
devLucasYuji <lucasyujideveloper@gmail.com>
2021-09-19 20:02:04 -0300

    use kotlin version from const

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Deps.Version.KOTLIN}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
