7ef8acb7cd46ab81b417c7c263941245fd7aa5a5
devLucasYuji <lucasyujideveloper@gmail.com>
2021-09-19 20:02:46 -0300

    add actions to advanced profile

--- app/src/main/java/br/com/crosslife/features/profile/view/AdvanceScreen.kt
+++ app/src/main/java/br/com/crosslife/features/profile/view/AdvanceScreen.kt
@@ -1,13 +1,14 @@
 package br.com.crosslife.features.profile.view
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import br.com.crosslife.R
 import br.com.crosslife.ui.components.topbar.ScaffoldTopbar
@@ -15,9 +16,43 @@ import br.com.crosslife.ui.components.topbar.ScaffoldTopbar
 @ExperimentalAnimationApi
 @Composable
 fun NavController.DetailProfileScreen() {
-    ScaffoldTopbar(titleRes = R.string.advanced_profile) {
+    ScaffoldTopbar(titleRes = R.string.advanced_profile, actions = { DetailProfileActions() }) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             Text(text = "Em construção", style = MaterialTheme.typography.h3)
+        }
+    }
+}
+
+// TODO should be deleted, only using 'cause of AC02
+@Composable
+fun DetailProfileActions() {
+    var showMenu by remember { mutableStateOf(false) }
+    IconButton(onClick = {}) {
+        Icon(imageVector = Icons.Filled.Share, contentDescription = "compartilhar")
+    }
+    IconButton(onClick = { showMenu = !showMenu }) {
+        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "menu kebab")
+    }
+    DropdownMenu(
+        expanded = showMenu,
+        modifier = Modifier.fillMaxWidth(0.5F),
+        onDismissRequest = { showMenu = false }
+    ) {
+        DropdownMenuItem(onClick = {}) {
+            Icon(
+                imageVector = Icons.Filled.Refresh,
+                contentDescription = "atualizar",
+                tint = Color.White,
+            )
+            Text("Atualizar")
+        }
+        DropdownMenuItem(onClick = {}) {
+            Icon(
+                imageVector = Icons.Filled.Settings,
+                contentDescription = "configuração",
+                tint = Color.White,
+            )
+            Text("Configuração")
         }
     }
 }
