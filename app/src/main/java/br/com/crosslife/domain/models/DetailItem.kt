36f306927f2f94c07fc24d2b9785084b0eb85e19
devLucasYuji <lucasyujideveloper@gmail.com>
2021-10-10 18:32:19 -0300

    fix subtitle to nullable

--- app/src/main/java/br/com/crosslife/domain/models/DetailItem.kt
+++ app/src/main/java/br/com/crosslife/domain/models/DetailItem.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class DetailItem(
     val title: String,
-    val subTitle: String,
+    val subTitle: String? = null,
     val description: String,
     val imageUrl: String,
 ): Parcelable
