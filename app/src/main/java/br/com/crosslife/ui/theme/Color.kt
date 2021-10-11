8492c52997135f3dabb93d9979297b0dfbf8ad5d
devLucasYuji <lucasyujideveloper@gmail.com>
2021-10-10 21:39:08 -0300

    fix black color from app

--- app/src/main/java/br/com/crosslife/ui/theme/Color.kt
+++ app/src/main/java/br/com/crosslife/ui/theme/Color.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val Green = Color(0xFF6BE067)
 val DarkGreen = Color(0xFF40B538)
-val Black = Color(0xFF22232C)
+val Black = Color(0xFF13161B)
 val White = Color(0xFFF2F2F2)
 val Gray = Color(0xFFA8A8A8)
 val DarkGray = Color(0xFFB4B1B1)
