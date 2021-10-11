6d78d9e2d0e2f8f02fd3f98a23dd85c030333d14
devLucasYuji <lucasyujideveloper@gmail.com>
2021-10-11 20:53:52 -0300

    add support to keyboard state

--- app/src/main/java/br/com/crosslife/ui/components/tabbar/TabBar.kt
+++ app/src/main/java/br/com/crosslife/ui/components/tabbar/TabBar.kt
@@ -1,12 +1,11 @@
 package br.com.crosslife.ui.components.tabbar
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalView
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import br.com.crosslife.Screen
@@ -14,14 +13,44 @@ import br.com.crosslife.ui.components.animation.FadeAnimation
 import br.com.crosslife.ui.components.animation.VerticalSlideAnimation
 import br.com.crosslife.ui.components.bottomnavigation.BottomNavigation
 import br.com.crosslife.ui.components.bottomnavigation.Tab
-import br.com.crosslife.ui.theme.Space
+import android.view.ViewTreeObserver
+
+
+enum class Keyboard {
+    Opened, Closed
+}
+
+@Composable
+fun keyboardAsState(): State<Keyboard> {
+    val keyboardState = remember { mutableStateOf(Keyboard.Closed) }
+    val view = LocalView.current
+    DisposableEffect(true) {
+        val onGlobalListener = ViewTreeObserver.OnGlobalLayoutListener {
+            val heightDiff = view.rootView.height - view.height
+            keyboardState.value = if (heightDiff > 100) {
+                Keyboard.Opened
+            } else {
+                Keyboard.Closed
+            }
+        }
+        view.viewTreeObserver.addOnGlobalLayoutListener(onGlobalListener)
+
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalListener)
+        }
+    }
+
+    return keyboardState
+}
 
 
 @ExperimentalAnimationApi
 @Composable
 fun NavController.TabBar(content: @Composable () -> Unit) {
     val currentTab by currentTabAsState()
-    val isTabNone = currentTab != Tab.None
+    val isKeyboardOpen by keyboardAsState()
+    val isTabNone = currentTab != Tab.None && isKeyboardOpen != Keyboard.Opened
+
     Scaffold(
         bottomBar = {
             VerticalSlideAnimation(visible = isTabNone) {
