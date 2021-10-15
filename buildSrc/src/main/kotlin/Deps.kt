52c9de3ff3c35237a8dbe5bda5c8d185c2c57e4f
devLucasYuji <lucasyujideveloper@gmail.com>
2021-10-15 01:08:48 -0300

    add flow layout dependency

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -110,6 +110,7 @@ object Deps {
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
         implementation("com.google.accompanist:accompanist-pager:0.13.0")
+        implementation("com.google.accompanist:accompanist-flowlayout:0.19.0")
         implementation("com.squareup.retrofit2:retrofit:${Version.RETROFIT}")
         implementation("com.squareup.retrofit2:converter-moshi:${Version.RETROFIT}")
         implementation("org.jetbrains.kotlin:kotlin-reflect:${Version.KOTLIN}")
