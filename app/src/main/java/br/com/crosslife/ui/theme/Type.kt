619ce58a821c94799abdb9f44aa2b98c27e46874
devLucasYuji <lucasyujideveloper@gmail.com>
2021-10-19 22:17:45 -0300

    change font family from nunito to poppins

--- app/src/main/java/br/com/crosslife/ui/theme/Type.kt
+++ app/src/main/java/br/com/crosslife/ui/theme/Type.kt
@@ -9,62 +9,62 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import br.com.crosslife.R
 
-val NunitoFamily = FontFamily(
-    Font(R.font.nunitobold, weight = FontWeight.Bold),
-    Font(R.font.nunitoregular, weight = FontWeight.Normal),
+val PoppinsFamily = FontFamily(
+    Font(R.font.poppinsbold, weight = FontWeight.Bold),
+    Font(R.font.poppinsregular, weight = FontWeight.Normal),
 )
 
 fun getTypography(colors: Colors) = Typography(
     h1 = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 48.sp,
         fontWeight = FontWeight.Bold,
         color = colors.onPrimary
     ),
     h2 = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 34.sp,
         fontWeight = FontWeight.Bold,
         color = colors.onPrimary
     ),
     h3 = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 24.sp,
         fontWeight = FontWeight.Bold,
         color = colors.onPrimary
     ),
     subtitle1 = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 16.sp,
         fontWeight = FontWeight.Bold,
         color = colors.onPrimary
     ),
     body1 = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 16.sp,
         fontWeight = FontWeight.Normal,
         color = colors.onPrimary
     ),
     body2 = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 14.sp,
         fontWeight = FontWeight.Normal,
         color = colors.onPrimary
     ),
     caption = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 12.sp,
         fontWeight = FontWeight.Normal,
         color = colors.onPrimary,
     ),
     button = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 14.sp,
         fontWeight = FontWeight.Normal,
         color = colors.onPrimary,
     ),
     overline = TextStyle(
-        fontFamily = NunitoFamily,
+        fontFamily = PoppinsFamily,
         fontSize = 10.sp,
         fontWeight = FontWeight.Normal,
         color = colors.onPrimary,
