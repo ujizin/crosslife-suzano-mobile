cf63bdc4a9375378163bb08ed6404cd6bb520f93
devLucasYuji <lucasyujideveloper@gmail.com>
2021-10-22 23:00:14 -0300

    add new logo

--- app/src/main/java/br/com/crosslife/features/home/view/HomeScreen.kt
+++ app/src/main/java/br/com/crosslife/features/home/view/HomeScreen.kt
@@ -1,9 +1,8 @@
 package br.com.crosslife.features.home.view
 
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -29,7 +28,7 @@ fun NavController.HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
     ) {
         SearchLazyColumn(searchFieldState,
             header = {
-                item { HomeLogo() }
+                item { Logo(Modifier.padding(top = Space.XXXS)) }
             }
         ) {
             item {
--- app/src/main/java/br/com/crosslife/features/home/view/Logo.kt
+++ app/src/main/java/br/com/crosslife/features/home/view/Logo.kt
@@ -1,34 +1,43 @@
 package br.com.crosslife.features.home.view
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import br.com.crosslife.R
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+import br.com.crosslife.ui.theme.Green
 import br.com.crosslife.ui.theme.Space
+import br.com.crosslife.ui.theme.White
 
 @Composable
-fun HomeLogo() {
+fun Logo(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = Space.BORDER),
         contentAlignment = Alignment.Center,
     ) {
-        Image(
-            modifier = Modifier
-                .padding(top = Space.XXS)
-                .size(56.dp, 52.dp),
-            painter = painterResource(
-                id = R.drawable.crosslife_logo),
-            contentDescription = stringResource(id = R.string.app_logo),
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(Green,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold)) {
+                    append("CROSS")
+                }
+                withStyle(style = SpanStyle(White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold)) {
+                    append("LIFE")
+                }
+            },
+            modifier = modifier
         )
     }
 }
