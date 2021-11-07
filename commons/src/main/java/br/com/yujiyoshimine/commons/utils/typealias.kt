57b271c09f7742eed5944089a38afea8802064ac
devLucasYuji <lucasyujideveloper@gmail.com>
2021-11-07 17:55:57 -0300

    Add new utils to commons

---
+++ commons/src/main/java/br/com/yujiyoshimine/commons/utils/typealias.kt
@@ -0,0 +1,8 @@
+package br.com.yujiyoshimine.commons.utils
+
+import br.com.yujiyoshimine.domain.model.Notice
+import br.com.yujiyoshimine.domain.model.WeeklyTrain
+
+typealias OnWeeklyTrainClick = (WeeklyTrain) -> Unit
+
+typealias OnNoticeClick = (Notice) -> Unit
