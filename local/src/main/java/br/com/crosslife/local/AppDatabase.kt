93ba8ff11b270a74d68f224b83e6f5037861126a
devLucasYuji <lucasyujideveloper@gmail.com>
2021-11-15 15:09:06 -0300

    add new notice entity & DAO

--- local/src/main/java/br/com/crosslife/local/AppDatabase.kt
+++ local/src/main/java/br/com/crosslife/local/AppDatabase.kt
@@ -2,17 +2,21 @@ package br.com.crosslife.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import br.com.crosslife.local.converter.Converters
+import br.com.crosslife.local.dao.NoticeDao
 import br.com.crosslife.local.dao.TrainDao
+import br.com.crosslife.local.entities.Notice
 import br.com.crosslife.local.entities.Train
 
 @Database(
-    entities = [
-        Train::class,
-    ],
+    entities = [Train::class, Notice::class],
     version = 1,
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun trainDao(): TrainDao
 
+    abstract fun noticeDao(): NoticeDao
 }
