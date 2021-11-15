8bb7bbfc18985c83a2906bc9d5a999c95ba23ac2
devLucasYuji <lucasyujideveloper@gmail.com>
2021-11-15 15:10:40 -0300

    Inject notice store

--- local/src/main/java/br/com/crosslife/local/LocalModules.kt
+++ local/src/main/java/br/com/crosslife/local/LocalModules.kt
@@ -2,9 +2,12 @@ package br.com.crosslife.local
 
 import android.content.Context
 import androidx.room.Room
+import br.com.crosslife.local.dao.NoticeDao
 import br.com.crosslife.local.dao.TrainDao
 import br.com.crosslife.local.datapreferences.DataPreferences
 import br.com.crosslife.local.datapreferences.DataPreferencesImpl
+import br.com.crosslife.local.store.notice.NoticeStore
+import br.com.crosslife.local.store.notice.NoticeStoreImpl
 import br.com.crosslife.local.store.train.TrainStore
 import br.com.crosslife.local.store.train.TrainStoreImpl
 import br.com.crosslife.local.store.user.UserStore
@@ -43,7 +46,15 @@ object LocalModules {
 
     @Singleton
     @Provides
+    internal fun provideNoticeDao(db: AppDatabase): NoticeDao = db.noticeDao()
+
+    @Singleton
+    @Provides
     fun provideTrainStore(trainDao: TrainDao): TrainStore = TrainStoreImpl(trainDao)
+
+    @Singleton
+    @Provides
+    fun provideNoticeStore(noticeDao: NoticeDao): NoticeStore = NoticeStoreImpl(noticeDao)
 
     @Singleton
     @Provides
