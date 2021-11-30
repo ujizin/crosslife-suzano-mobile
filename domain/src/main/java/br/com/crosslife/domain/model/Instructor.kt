042eed57a8f504bd3fe442824356637fb3c42f80
devLucasYuji <lucasyujideveloper@gmail.com>
2021-11-30 01:25:06 -0300

    Fix id from int to string on Instructor

--- domain/src/main/java/br/com/crosslife/domain/model/Instructor.kt
+++ domain/src/main/java/br/com/crosslife/domain/model/Instructor.kt
@@ -5,7 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Instructor(
-    val id: Int,
+    val id: String,
     val name: String,
     val number: Int,
     val imageUrl: String
@@ -17,14 +17,14 @@ data class Instructor(
 }
 
 val InstructorOne = Instructor(
-    1,
+    "instrutor01",
     "Carlos",
     1,
     imageUrl = "https://armasterplus.com.br/wp-content/uploads/bfi_thumb/our-team-4-1-36g5tt9lpndi5omesjwtfu@2x.jpg"
 )
 
 val InstructorTwo = Instructor(
-    1,
+    "instrutor02",
     "Bruno",
     2,
     imageUrl = "https://5ba9963e93153e603e786438.static-01.com/l/images/a3bb662d840efe6219c2ff7d8099da8af3b6c41f.jpg"
