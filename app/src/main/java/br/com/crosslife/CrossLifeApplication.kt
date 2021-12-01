34345cadd6304d64a9726a4333d5c64ccffad877
devLucasYuji <lucasyujideveloper@gmail.com>
2021-12-01 00:21:58 -0300

    Create channel whenever user open app

--- app/src/main/java/br/com/crosslife/CrossLifeApplication.kt
+++ app/src/main/java/br/com/crosslife/CrossLifeApplication.kt
@@ -4,4 +4,9 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CrossLifeApplication : Application()
+class CrossLifeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrossLifeMessageService.createNotificationChannel(this)
+    }
+}
